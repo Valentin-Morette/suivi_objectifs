ea51da75020f817f51d28862afaecc0857ff986e
Valentin-Morette <morette.valentin@gmail.com>
2026-06-13 11:32:42 +0200

    commit test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
